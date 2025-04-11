67f99a18574526905b27d88cb50a017f15c4cec6
Sean Poduska <spoduska@ucsc.edu>
2025-04-10 23:05:30 -0700

    Completed hw1.c and caat.c

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -6,43 +6,31 @@
 
 // For the value of Pi, please use M_PI, which is defined in math.h.
 double area_of_disk(double radius) {
-  // your code here
-
-  // TODO: remove all these UNUSED things when you actually write the function.
-  UNUSED(radius);
-  return 0;
+  return (M_PI * (radius * radius));
 }
 
-double area_of_ring(double outer_radius, double inner_radius) {
-  // your code here
-
-  UNUSED(outer_radius);
-  UNUSED(inner_radius);
-  return 0;
+double area_of_ring(double outer_radius, double inner_radius) {   
+  return area_of_disk(outer_radius) - area_of_disk(inner_radius);
 }
 
 int bigger_minus_smaller(int a, int b) {
-  // your code here
-
-  UNUSED(a);
-  UNUSED(b);
-  return 0;
+  return (a > b) ? (a - b) : (b - a);
 }
 
 bool value_in_range(int lower_bound, int x, int upper_bound) {
-  // your code here
-
-  UNUSED(lower_bound);
-  UNUSED(upper_bound);
-  UNUSED(x);
-  return false;
+  if (x >= lower_bound && x <= upper_bound){
+  	return true;
+  }else{
+	return false;
+  }
 }
 
 int sum_of_greater_squares(int a, int b, int c) {
-  // your code here
-
-  UNUSED(a);
-  UNUSED(b);
-  UNUSED(c);
-  return false;
+  if ((a * a) < (b * b) && (a * a) < (c * c)){
+	return ((b * b) + (c * c));
+  }else if ((b * b) < (a * a) && (b * b) < (c * c)){
+	return ((a * a) + (c * c));
+  }else{
+	return ((a * a) + (b * b));
+  }
 }
