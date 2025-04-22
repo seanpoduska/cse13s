1b7089ae6956a8e90c5980a445bc6ca8eabb4535
Sean Poduska <spoduska@ucsc.edu>
2025-04-21 17:37:48 -0700

    Uploading hw2.c and calculator.c

--- hw2/calculator.c
+++ hw2/calculator.c
@@ -2,40 +2,64 @@
 #include <stdlib.h>
 
 void remove_newline(char *s, int maxsize) {
-  for (int i = 0; i < maxsize && s[i]; i++) {
-    if (s[i] == '\n') {
-      s[i] = '\0';
-      return;
-    }
-  }
+	for (int i = 0; i < maxsize && s[i]; i++) {
+		if (s[i] == '\n') {
+			s[i] = '\0';
+			return;
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
-  const int BUFSIZE = 80;
-  char buffer[BUFSIZE];
-  FILE *infile = NULL;
+	const int BUFSIZE = 80;
+	char buffer[BUFSIZE];
+	FILE *infile = NULL;
 
-  if (argc != 2) {
-    fprintf(stderr, "calculator needs exactly 1 argument, the input file\n");
-    return 1;
-  }
-  infile = fopen(argv[1], "r");
-  if (!infile) {
-    fprintf(stderr, "input file %s not found\n", argv[1]);
-    return 1;
-  }
+	if (argc != 2) {
+		fprintf(stderr, "calculator needs exactly 1 argument, the input file\n");
+		return 1;
+	}
+	infile = fopen(argv[1], "r");
+	if (!infile) {
+		fprintf(stderr, "input file %s not found\n", argv[1]);
+		return 1;
+	}
 
-  // Your code should go here-ish. Feel free to change this loop as you need to.
-  // IMPORTANT: your output should either be the single line "ERROR\n", or the
-  // single number output, and nothing else.
-  while (fgets(buffer, BUFSIZE, infile)) {
-    // TODO: implement the calculator.
-    remove_newline(buffer, BUFSIZE);
+	// Your code should go here-ish. Feel free to change this loop as you need to.
+	// IMPORTANT: your output should either be the single line "ERROR\n", or the
+	// single number output, and nothing else.
+	long res = 0;
+	long num;
+	char op;
+	while (fgets(buffer, BUFSIZE, infile)) {
+    	if (sscanf (buffer, "%li", &num) == 1) { //reads buffer to find number, format as long
+			switch (op) {
+				case '+':
+					res += num;
+					break;
+				case '-':
+					res -= num;
+					break;
+				case '*':
+					res *= num;
+					break;
+				case '/':
+					if (num == 0) {
+						printf("ERROR\n");
+						return 1;
+					} else {
+						res /= num;
+					}
+					break;
+				default:
+					res = num;
+			}
+		} else {
+			op = buffer[0];
+		}	
+	}
 
-    // You should remove the following line. This is just to show that we are
-    // reading in lines from the input file.
-    printf("got this line: %s\n", buffer);
-  }
-
-  return 0;
-}
+	remove_newline(buffer, BUFSIZE);
+	printf("%li\n", res);
+	return 0;
+}	
--- hw2/hw2.c
+++ hw2/hw2.c
@@ -8,29 +8,50 @@
 #include "hw2.h"
 
 int next_collatz_number(int n) {
-  UNUSED(n);
-  return 0;
+	if (n % 2 == 1) {
+		return n * 3 + 1;
+	} else {
+		return n / 2;
+	}
+	return 0;
 }
 
 int count_collatz_steps(int n) {
-  UNUSED(n);
-  return 0;
+	int count = 0;
+	int num = n;
+	while (num != 1) {
+		count += 1;
+		num = next_collatz_number(num);
+	}
+	return count;
 }
 
 int maximum(int *nums, int len) {
-  UNUSED(nums);
-  UNUSED(len);
-  return 0;
+	int max = nums[0];
+	for (int i = 1; i < len; i++) {
+		if (nums[i] > max) {
+			max = nums[i];
+		}
+	}
+	return max;
 }
 
 int sum_positive(int *nums, int len) {
-  UNUSED(nums);
-  UNUSED(len);
-  return 0;
+	int sum = 0;
+	for (int i = 0; i < len; i++) {
+		if (nums[i] > 0) {
+			sum = sum + nums[i];
+		}
+	}
+	return sum;
 }
 
-int count_negative(int *nums, int len) {
-  UNUSED(nums);
-  UNUSED(len);
-  return 0;
-}
+int count_negative(int *nums, int len) {	
+	int count = 0;
+	for (int i = 0; i < len; i++) {
+		if (nums[i] < 0) {
+			count += 1;
+		}
+	}
+	return count;
+}	
