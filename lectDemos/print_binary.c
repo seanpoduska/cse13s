0ff7b5b2c8a2b3ea3c95df3de2782212a0964b81
Sean Poduska <spoduska@ucsc.edu>
2025-05-05 19:39:32 -0700

    lectDemos Push

---
+++ lectDemos/print_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_binary(intx) {
+	int query;
+	for (int position = 31; position >= 0; position--) {
+		
+	}
+}
+
+
+void print_binary_string(char *text) {
+	for (char *c = text; *c; c++) {
+		for (int position) {
+			
+		}
+	}
+}
+
+void main() {
+	
+}
